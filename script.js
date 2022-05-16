4f025e288703f352325422340814dba0ec3bcd2b
XIYO <krb7korea@me.com>
2022-05-16 17:30:57 +0900

    여러 코드 수정

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+let hamberger;
+let sidebar;
+let dimmed;
+let sidebarContainer;
+
 window.onload = () => {
-    var hamberger = document.querySelector('#hamberger');
-    var sidebar = document.querySelector('#sidebar');
-    var contents = document.querySelectorAll('#contents> .content');
+    hamberger = document.querySelector('#hamberger');
+    sidebarContainer = document.querySelector('#sidebarContainer');
+    sidebar = document.querySelector('#sidebar');
+    dimmed = document.querySelector('#dimmed');
 
     hamberger.addEventListener('click', () => {
-        hamberger.firstElementChild.classList.toggle('rotateR');
-        hamberger.lastElementChild.classList.toggle('rotateL');
-        sidebar.classList.toggle('active');
-        contents.forEach(v => v.classList.toggle('active'));
+        hambergerClick();
     })
+
+    dimmed.addEventListener('click', () => {
+        hambergerClick();
+    })
+}
+
+function hambergerClick() {
+    hamberger.firstElementChild.classList.toggle('rotateR');
+    hamberger.lastElementChild.classList.toggle('rotateL');
+    sidebar.classList.toggle('active');
+    dimmed.classList.toggle('active');
+    sidebarContainer.classList.toggle('active');
 }

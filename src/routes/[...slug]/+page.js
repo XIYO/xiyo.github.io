ddc156e762d97d4a73e37146816531e34e9b1f07
이요 <krb7korea@me.com>
2024-04-12 04:36:02 +0000

    simply

--- src/routes/[...slug]/+page.js
+++ src/routes/[...slug]/+page.js
@@ -4,10 +4,10 @@ import Category from "$lib/post/Category.js";
 import Post from "$lib/post/Post.js";
 
 export async function load({url}) {
-    const result = {};
-    result.post = Post.getPosts(url.pathname);
-    result.category = Category.getCategory(url.pathname);
-    return result;
+    return {
+        post: Post.getPosts(url.pathname),
+        category: Category.getCategory(url.pathname)
+    }
 
     // return error(404, `Could not find ${url.pathname}`);
 }

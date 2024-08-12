686434111af3633a393ef940fdbb1a5124f2b97c
XIYO <master@xiyo.dev>
2024-08-12 14:35:21 +0900

    :art: .md 파싱 플러그인 만들어서 대체함, Post와 category가 너무 많은 역할이 있었음.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-
+import vitePluginXiyo from './src/lib/plugin/vitePluginXiyo.js';
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit()]
+	plugins: [enhancedImages(), sveltekit(), vitePluginXiyo()]
 });

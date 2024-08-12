560f6d0735a0b40073c3fe580ff67ce83be48edc
XIYO <master@xiyo.dev>
2024-08-13 04:55:55 +0900

    :art: view-transition 최신 문법인 클래스 추가

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,12 @@
-<footer style="--view-transition-name: footer">Designed By chimi 🎈</footer>
+<footer>Designed By chimi 🎈</footer>
 
 <style>
 	footer {
 		font-size: 0.75rem;
 		text-align: center;
 		margin-block: var(--default-margin-block);
+
+			view-transition-name: footer;
+			view-transition-class: after-view-transition;
 	}
 </style>

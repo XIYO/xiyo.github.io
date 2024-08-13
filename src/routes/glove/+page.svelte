9cec0fc09f0c67ec279f906801c1d073aba790eb
XIYO <master@xiyo.dev>
2024-08-13 20:00:06 +0900

    :art: 설명 추가

--- src/routes/glove/+page.svelte
+++ src/routes/glove/+page.svelte
@@ -3,32 +3,38 @@
 	import jjakak from '$lib/assets/jjakak.jpeg?enhanced';
 </script>
 
-<Border viewTransitionName="content" padding negative id="content">
-	<a class="glove content" href="https://jjakak.xiyo.dev" target="_blank">
-		<enhanced:img src={jjakak} alt="째깍 타이머 미리보기" />
-		<h2>JJaKaK</h2>
-	</a>
+<Border viewTransitionName="article" negative id="article">
+	<div class="highlight-background padding">
+		글러브는 제가 만든 프로젝트들을 모아놓은 공간입니다.
+	</div>
+	<div class="gloves padding">
+		<a class="glove content" href="https://jjakak.xiyo.dev" target="_blank">
+			<enhanced:img src={jjakak} alt="째깍 타이머 미리보기" />
+			<h2>JJaKaK</h2>
+			<p>심플 타이머 입니다. PWA로 구현로 구현되어 있어 브라우저에서 앱으로 바로 설치가 가능합니다.</p>
+		</a>
+	</div>
 </Border>
 
 <style>
-	:global(#border-inner-content) {
-		display: grid;
+    .gloves {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
 
-		grid-template-columns: repeat(3, 1fr);
+        & > .glove {
+            text-decoration: none;
+            text-align: center;
 
-		:global(&) > .glove {
-			img {
-				object-fit: cover;
-				aspect-ratio: 1;
-				inline-size: 100%;
-				block-size: auto;
-			}
+            img {
+                object-fit: cover;
+                aspect-ratio: 1;
+                inline-size: 100%;
+                block-size: auto;
+            }
 
-			h2 {
-				text-align: center;
-
-				margin-block: 0.5rem;
-			}
-		}
-	}
+            h2 {
+                margin-block: 0.25rem;
+            }
+        }
+    }
 </style>

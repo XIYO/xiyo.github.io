d9debf8a05961cf7730c60c286131946c2fbc05b
XIYO <master@xiyo.dev>
2024-08-15 07:48:01 +0900

    :bug: 잘못된 참조 수정

--- src/lib/components/Posts.svelte
+++ src/lib/components/Posts.svelte
@@ -28,7 +28,7 @@
 		{#each category.posts as post}
 			<li>
 				<article>
-					<a href={post.absolutePath}>{post.frontmatter.title}</a>
+					<a href={post.absolutePath}>{post.title}</a>
 				</article>
 			</li>
 		{/each}

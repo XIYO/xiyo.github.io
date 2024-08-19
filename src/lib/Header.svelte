d5c51a784cf977b256073a11a63230f873e801d8
XIYO <master@xiyo.dev>
2024-08-20 02:23:21 +0900

    :art: NavButton.svelte 으로 내비게이션 호출 버튼을 컴포넌트화

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Border from '$lib/ui/Border.svelte';
+	import NavButton from '$lib/components/NavButton.svelte';
 
 	const { title, tag = 'h1' } = $props();
 </script>
@@ -7,13 +8,7 @@
 <Border id="header" viewTransitionName="header" tag="header">
 	<h1>{title}</h1>
 
-	<button
-		aria-label="Toggle navigation"
-		popovertarget="border-outer-nav"
-		popovertargetaction="toggle"
-	>
-		<span class="no-css-only">메뉴 토글</span>
-	</button>
+	<NavButton popovertargetaction="toggle"/>
 </Border>
 
 <style>
@@ -30,48 +25,15 @@
 		h1 {
 			/* reset */
 			margin-block: 0;
-			margin-inline: var(--default-padding);
+
+			padding: var(--default-padding);
+			margin-block-end: 4px; /* 스크롤이 바닥에 붙지 않기 위해 */
 
 			align-content: center;
 			text-transform: uppercase;
 
 			overflow-x: scroll; /* 의도적으로 스크롤을 노출하여 스크롤 높이도 미리 계산 하도록 한다 */
 			text-wrap: nowrap;
-		}
-
-		button {
-			/* reset */
-			border: unset;
-			padding: unset;
-
-			block-size: 100%;
-			aspect-ratio: 1;
-			background-color: var(--color-default-white);
-
-			&::after {
-				content: '⛱️';
-				display: inline-block;
-				font-size: 1.5rem;
-			}
-		}
-	}
-
-	@media (prefers-reduced-motion: no-preference) {
-		button {
-			transition: opacity var(--default-animation-duration);
-		}
-	}
-
-	@media (min-width: 769px) {
-		button {
-			pointer-events: none;
-			opacity: 0;
-		}
-	}
-
-	@media (max-width: 768px) {
-		button {
-			cursor: pointer;
 		}
 	}
 </style>
---
+++ src/lib/components/NavButton.svelte
@@ -0,0 +1,53 @@
+<script>
+	/**
+	 * @type {{ popovertargetaction: 'toggle' | 'hide' | 'show' | null | undefined }}
+	 */
+	const { popovertargetaction } = $props();
+</script>
+<button
+	aria-label="Toggle navigation"
+	popovertarget="border-outer-nav"
+	popovertargetaction={popovertargetaction}
+>
+	<span class="no-css-only">menu {popovertargetaction}</span>
+</button>
+
+
+<style>
+    button {
+        /* reset */
+        border: unset;
+        padding: unset;
+
+        block-size: 100%;
+        aspect-ratio: 1;
+        background-color: var(--color-default-white);
+
+        &::after {
+            content: '⛱️';
+            display: inline-block;
+            font-size: 1.5rem;
+        }
+
+        @media (prefers-reduced-motion: no-preference) {
+            transition: opacity var(--default-animation-duration);
+        }
+
+        @media (768px < width) {
+            pointer-events: none;
+            opacity: 0;
+        }
+
+        @media (width <= 768px) {
+            cursor: pointer;
+        }
+    }
+
+		:global(nav) button::after {
+				content: '🏖️';
+		}
+
+    .no-css-only {
+        display: none;
+    }
+</style>

5c005f64097c5815d39abcf5daa55c2bb0638a3b
XIYO <master@xiyo.dev>
2024-08-23 08:07:29 +0900

    :lipstick: 내비게이션 토글 버튼 쪼그라 드는 문제 수정

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -14,13 +14,17 @@
 <style>
 	:global(#border-outer-header) {
 		margin-block: var(--default-margin);
+		block-size: 4rem;
 	}
 
 	:global(#border-content-header) {
 		display: flex;
 		justify-content: space-between;
+		align-items: center;
 
 		h1 {
+			flex-grow: 1;
+
 			/* reset */
 			margin-block: 0;
 

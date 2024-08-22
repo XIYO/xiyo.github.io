35c869c8527c4c71261425c043ab9aa266c4cdd1
XIYO <master@xiyo.dev>
2024-08-22 13:39:43 +0900

    Revert ":art: shiki는 스타일을 인라인으로 만들어서 맘에 안들어서 highlightjs로 변경"
    This reverts commit 3cc3e926cbdbcf8fd71ecf0ebce03b2add3d440d.

--- tests/categories-posts/category.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect, beforeEach } from 'vitest';
-import Category from '/src/lib/post/Category';
-
-describe('카테고리 테스트', () => {
-	it('카테고리 초기화 테스트', async () => {
-		const root = Category.root;
-		await root.toSerialize();
-	});
-})

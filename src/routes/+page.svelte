7a4d7f622836b707eab91bf4980a54e335784155
XIYO <master@xiyo.dev>
2024-08-31 19:30:02 +0900

    :memo: 멘트 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,6 @@
 <Border viewTransitionName="article" negative>
 	<div class="padding content">
 		<p>no js! no css! 환경을 고려한! svelte로 만든 페이지입니다.</p>
+		<p>자바스크립트가 활성화 되어 있으면 반응성이 향상됩니다!</p>
 	</div>
 </Border>

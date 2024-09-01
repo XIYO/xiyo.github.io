5c92c1ee1c720883abcc615128a74d44ed6ca071
XIYO <master@xiyo.dev>
2024-09-01 20:50:34 +0900

    :globe_with_meridians: 국제화 문장으로 출력되게 변경

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
 	import Border from '$lib/ui/Border.svelte';
+	import * as m from '$lib/paraglide/messages.js';
 </script>
 
 <Border viewTransitionName="article" negative>
 	<div class="padding content">
-		<p>no js! no css! 환경을 고려한! svelte로 만든 페이지입니다.</p>
-		<p>자바스크립트가 활성화 되어 있으면 반응성이 향상됩니다!</p>
+		<p>{m.welcome()}</p>
 	</div>
 </Border>

26421a48623ced0db25aa29541f806e3672f94b6
XIYO <master@xiyo.dev>
2024-09-05 23:17:17 +0900

    :wrench: @lobehub/i18n-cli 자동 번역 기능 설정 추가

---
+++ .i18nrc.cjs
@@ -0,0 +1,28 @@
+const { defineConfig } = require('@lobehub/i18n-cli');
+
+module.exports = defineConfig({
+	entry: 'markdown/ko-KR',
+	entryLocale: 'ko-KR',
+	output: 'markdown',
+	outputLocales: [
+		'zh-TW',
+		'en-US',
+		'ja-JP',
+	],
+	// temperature: 0,
+	// modelName: 'gpt-4o',
+	// splitToken: 2048,
+	experimental: {
+		jsonMode: true,
+	},
+	markdown: {
+		entry: ['static/**/*'],
+		entryLocale: 'ko-KR',
+		entryExtension: '.md',
+		exclude: ['**/*.en-US.md'],
+		// exclude: ['markdown/en-US', 'markdown/zh-CN', 'markdown/ja-JP'],
+		outputLocales: [
+			'en-US'
+		],
+	}
+});

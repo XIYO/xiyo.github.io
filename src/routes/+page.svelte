49e2a68ba9f59dcca3434da298d730cad2c273e3
XIYO <bunny@xiyo.dev>
2024-09-28 23:53:31 +0900

    :art: 터미널 실행 예제 이미지 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script>
 	import Border from '$lib/ui/Border.svelte';
 	import * as m from '$lib/paraglide/messages.js';
+	import terminalBrowsing from '$lib/assets/terminal_browsing.png?enhanced';
 </script>
 
 <Border viewTransitionName="post" negative>
 	<div class="padding content">
-		<p>{m.welcome()}</p>
+		<p>{@html m.welcome()}</p>
+
+		<figure>
+		  <enhanced:img src={terminalBrowsing} alt={m.terminalBrowsing()} data-title={m.terminal()}/>
+			<figcaption>
+				터미널 브라우저에서 실행한 화면
+			</figcaption>
+		</figure>
 	</div>
 </Border>

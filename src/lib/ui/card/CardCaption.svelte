c8ba0f7112b184970ebd17b4c598a512e7790ad2
XIYO <bunny@xiyo.dev>
2025-02-16 15:18:33 +0900

    :art: add CardCaption component to render children with styling

---
+++ src/lib/ui/card/CardCaption.svelte
@@ -0,0 +1,16 @@
+<script>
+	const { children } = $props();
+</script>
+
+<div class="highlight-background">
+	{@render children()}
+</div>
+
+<style>
+	div {
+		/* reset */
+		margin-block: 0;
+
+		padding: var(--default-padding);
+	}
+</style>

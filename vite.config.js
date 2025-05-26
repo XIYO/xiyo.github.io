b4e33e79562eaab8d671bb73382f3cf9e02c4106
XIYO <bunny@xiyo.dev>
2025-05-26 19:01:00 +0900

    Add Tailwind CSS plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import vitePluginXiyo from './src/lib/plugin/vitePluginXiyo.js';
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
 		enhancedImages(),
 		sveltekit(),
+		tailwindcss(),
 		vitePluginXiyo(),
 		paraglideVitePlugin({
 			project: './project.inlang',

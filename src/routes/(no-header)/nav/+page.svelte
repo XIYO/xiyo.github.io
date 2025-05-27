4096f8a826baad4566fd7c18e25ddb9dc515db72
XIYO <bunny@xiyo.dev>
2025-05-27 10:32:18 +0900

    :art: 따옴표 스타일 통일을 위한 Nav 컴포넌트 임포트 수정

--- src/routes/(no-header)/nav/+page.svelte
+++ src/routes/(no-header)/nav/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Nav from '$lib/components/Nav.svelte';
+	import Nav from "$lib/components/Nav.svelte";
 </script>
 
 <div class="h-screen w-md mx-auto flex justify-center items-center">

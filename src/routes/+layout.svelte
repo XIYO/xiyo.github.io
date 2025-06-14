10d91326c794169346ee15a2a747d3033f3c974a
XIYO <bunny@xiyo.dev>
2025-06-14 15:20:04 +0900

    ⚰️

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,6 @@
 <script>
 	import { page } from '$app/state';
-
-	// components
-	import Nav from '$lib/components/Nav.svelte';
-	import Footer from '$lib/components/Footer.svelte';
-	import Header from '$lib/Header.svelte';
-
-	// styles
 	import '$lib/stylesheet/app.css';
-
 	import '$lib/stylesheet/tailwind.css';
 
 	const { children } = $props();

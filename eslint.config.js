14d9a6f033d5c9ca8eb3d6d7ad5ec1914372512e
XIYO <bunny@xiyo.dev>
2025-06-15 18:29:01 +0900

    refactor: ESLint 설정 최적화 - 중복 코드 제거 및 효율적인 ignore 패턴 적용

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,6 @@ import svelte from 'eslint-plugin-svelte';
 import prettier from 'eslint-config-prettier';
 import globals from 'globals';
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
 	js.configs.recommended,
 	...svelte.configs['flat/recommended'],
@@ -28,10 +27,8 @@ export default [
 			'.obsidian/',
 			'playwright.config.cjs',
 			'node_modules/',
-			'storybook-static/',
-			'storybook-static/**/*'
+			'storybook-static/**'
 		]
 	},
-	...storybook.configs['flat/recommended'],
 	...storybook.configs['flat/recommended']
 ];

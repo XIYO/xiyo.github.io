702790eb02883e32fba0568c3499212793acda1d
XIYO <bunny@xiyo.dev>
2025-10-19 04:07:13 +0900

    fix: update semantic-release config for pnpm

--- .releaserc.js
+++ .releaserc.js
@@ -22,7 +22,7 @@ export default {
 		[
 			'@semantic-release/git',
 			{
-				assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+				assets: ['CHANGELOG.md', 'package.json', 'pnpm-lock.yaml'],
 				message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
 			}
 		],
